0b2fb311493de22ba1e7cd0e45eacf27724dafdd
Jake <jakemathias@hotmail.com>
2023-09-11 19:41:28 -0500

    ValidAnagram

---
+++ arrays_strings/grind75/4.ValidAnagram.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+# https://leetcode.com/problems/valid-anagram/
+# Tags: hashing, hashmap
+class Solution:
+
+    # Runtime Complexity: O(n)
+    # Space Complexity: O()
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        # this allows us to default values to 0
+        d = defaultdict(int)
+        
+        for c in s:
+            d[c] += 1
+
+        for c in t:
+            d[c] -= 1
+
+        for val in d.values():
+            if val != 0:
+                return False
+            
+        return True
+
+        
+solution = Solution()
+answer = solution.isAnagram("rat", "car") # valid
+answer = solution.isAnagram("anagram", "nagaram") # invalid
+print(answer)
