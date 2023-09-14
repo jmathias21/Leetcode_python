773928f379d16f38e4c91c74c51dda1618da802e
Jake <jakemathias@hotmail.com>
2023-09-13 20:23:54 -0500

    uncomment

--- arrays_strings/grind75/6.MajorityElement.py
+++ arrays_strings/grind75/6.MajorityElement.py
@@ -23,6 +23,6 @@ class Solution:
         
 solution = Solution()
 answer = solution.majorityElement([12,52,12,70,12,61,12,12,50,72,82,12,11,25,28,43,40,12,12,53,12,12,98,12,12,92,81,2,12,15,40,12,12,9,12,31,12,12,12,12,77,12,12,50,12,12,12,93,41,92,12,12,12,12,12,12,12,12,12,37,48,14,12,70,82,12,80,12,12,12,12,56,30,12,8,12,50,12,20,12,21,97,12,42,12,10,12,38,73,15,9,11,79,12,12,28,51,12,15,12])
-#answer = solution.majorityElement([2,3,3])
-#answer = solution.majorityElement([2,2,4,3,1,1,2,2,2])
+answer = solution.majorityElement([2,3,3])
+answer = solution.majorityElement([2,2,4,3,1,1,2,2,2])
 print(answer)
