55424e923415e3ce50403283907dad2f145edf72
Jake <jakemathias@hotmail.com>
2023-09-20 14:39:10 -0500

    Add another TwoSum solution

--- arrays_strings/grind75/1.TwoSum.py
+++ arrays_strings/grind75/1.TwoSum.py
@@ -21,9 +21,28 @@ class Solution:
             hashMap[target - num] = i
 
         return []
+    
+    # Runtime Complexity: O(n log n)
+    # Space Complexity: O(1)
+    def twoSumUsingTwoPointers(self, nums: List[int], target: int) -> List[int]:
+        nums.sort() # worst case of O(n log n)
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            s = nums[left] + nums[right]
+            if s < target:
+                left += 1
+            elif s > target:
+                right -= 1
+            else:
+                return [left + 1, right + 1]
+
 
         
 solution = Solution()
+answer = solution.twoSumUsingTwoPointers([2,7,11,15], 9)
+
 answer = solution.twoSum([-1,-2,-3,-4,-5], -8)
 print(answer)
 
