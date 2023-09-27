6faa1196fce79322310d854488448fba55fdb411
Jake <jakemathias@hotmail.com>
2023-09-27 14:31:40 -0500

    Add backtracking solution for CombinationSum

--- arrays_strings/grind75/16.CombinationSum.py
+++ arrays_strings/grind75/16.CombinationSum.py
@@ -1,8 +1,30 @@
 from typing import List
 
 # https://leetcode.com/problems/combination-sum/
-# Tags: dynamic programming
+# Tags: backtracking, dynamic programming, DFS
 class Solution:
+
+    # Runtime Complexity: O(2^n)
+    # Space Complexity: O(t)
+    # Time: Not timed
+    def combinationSumUsingBacktrackingRec(self, candidates, target):
+        answer = []
+
+        def dfs(i, cur, sum):
+            if sum == target:
+                answer.append(cur.copy())
+                return
+            if i >= len(candidates) or sum > target:
+                return
+
+            cur.append(candidates[i])
+            dfs(i, cur, sum + candidates[i]) # left
+            cur.pop()
+            dfs(i + 1, cur, sum) # right
+
+        dfs(0, [], 0)
+        return answer
+        
 
     # Runtime Complexity: O()
     # Space Complexity: O()
@@ -17,24 +39,30 @@ class Solution:
     # us [2,2,3]
     def combinationSumUsingDP(self, candidates, target):
         dp = [[] for _ in range(target + 1)]
-        dp[0].append([])
-        
+        dp[0] = [[]]
+
         for candidate in candidates:
             for i in range(candidate, target + 1):
+                # find previous combinations that we know are equal to i (the current target)
+                # minus the candidate. This gives us a combination that, when we apply our
+                # candidate, will add up exactly to the target
                 for combination in dp[i - candidate]:
                     dp[i].append(combination + [candidate])
-                
+
         return dp[target]
 
         
 solution = Solution()
-answer = solution.combinationSum([2,3,6,7], 7)
+answer = solution.combinationSumUsingBacktrackingRec([8,7,4,3], 11)
+#answer = solution.combinationSumUsingBacktrackingRec([2,3,6,7], 7)
 print(answer)
+
+#
 
 # Example: combinationSumUsingDP
 # [2,3,6,7] target = 7
 
-# [2,3,6,7] number = 2
+# [2,3,6,7] candidate = 2
 # target = 1, []
 # target = 2, [2]
 # target = 3, []
@@ -43,7 +71,7 @@ print(answer)
 # target = 6, [2,2,2]
 # target = 7, []]
 
-# [2,3,6,7] number = 3
+# [2,3,6,7] candidate = 3
 # target = 1, []
 # target = 2, [2]
 # target = 3, [3]
@@ -52,7 +80,7 @@ print(answer)
 # target = 6, [2,2,2], [3,3]
 # target = 7, [2,2,3]
 
-# [2,3,6,7] number = 6
+# [2,3,6,7] candidate = 6
 # target = 1, []
 # target = 2, [2]
 # target = 3, [3]
@@ -61,7 +89,7 @@ print(answer)
 # target = 6, [2,2,2], [3,3], [6]
 # target = 7, [2,2,3]
 
-# [2,3,6,7] number = 7
+# [2,3,6,7] candidate = 7
 # target = 1, []
 # target = 2, [2]
 # target = 3, [3]
