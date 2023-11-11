792e774ff01d5b8e85a51a3673207e352053c8bc
Jake <jakemathias@hotmail.com>
2023-11-11 07:48:02 -0600

    Tweak

--- trees_graphs/grind75/46.WordLadder.py
+++ trees_graphs/grind75/46.WordLadder.py
@@ -1,11 +1,6 @@
 from collections import defaultdict, deque
 from typing import List
 
-class TreeNode:
-    def __init__(self):
-        self.val = None
-        self.children = {}
-
 # https://leetcode.com/problems/word-ladder/
 # Tags: BFS, Graph
 class Solution:
